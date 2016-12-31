d9e7a15657d2e63fceeb16e50c853df0d2bf6af8
mepeisen <mep_eisen@web.de>
2016-12-31 13:23:37 +0100

    Scoreboard fixes

--- src/main/java/com/comze_instancelabs/conquer/IArenaScoreboard.java
+++ src/main/java/com/comze_instancelabs/conquer/IArenaScoreboard.java
@@ -29,34 +29,34 @@ public class IArenaScoreboard extends ArenaScoreboard {
 	public void updateScoreboard(final IArena arena) {
 		for (String p_ : arena.getAllPlayers()) {
 			Player p = Bukkit.getPlayer(p_);
-			if (!ascore.containsKey(arena.getName())) {
-				ascore.put(arena.getName(), Bukkit.getScoreboardManager().getNewScoreboard());
+			if (!ascore.containsKey(arena.getInternalName())) {
+				ascore.put(arena.getInternalName(), Bukkit.getScoreboardManager().getNewScoreboard());
 			}
-			if (!aobjective.containsKey(arena.getName())) {
-				aobjective.put(arena.getName(), ascore.get(arena.getName()).registerNewObjective(arena.getName(), "dummy"));
+			if (!aobjective.containsKey(arena.getInternalName())) {
+				aobjective.put(arena.getInternalName(), ascore.get(arena.getInternalName()).registerNewObjective(arena.getInternalName(), "dummy"));
 			}
 
-			aobjective.get(arena.getName()).setDisplaySlot(DisplaySlot.SIDEBAR);
+			aobjective.get(arena.getInternalName()).setDisplaySlot(DisplaySlot.SIDEBAR);
 
-			aobjective.get(arena.getName()).setDisplayName(MinigamesAPI.getAPI().pinstances.get(plugin).getMessagesConfig().scoreboard_title.replaceAll("<arena>", arena.getName()));
+			aobjective.get(arena.getInternalName()).setDisplayName(MinigamesAPI.getAPI().pinstances.get(plugin).getMessagesConfig().scoreboard_title.replaceAll("<arena>", arena.getDisplayName()));
 
-			ascore.get(arena.getName()).resetScores(Bukkit.getOfflinePlayer(Integer.toString(arena.redcp - 1) + " "));
-			ascore.get(arena.getName()).resetScores(Bukkit.getOfflinePlayer(Integer.toString(arena.redcp + 1) + " "));
-			ascore.get(arena.getName()).resetScores(Bukkit.getOfflinePlayer(Integer.toString(arena.bluecp - 1) + "  "));
-			ascore.get(arena.getName()).resetScores(Bukkit.getOfflinePlayer(Integer.toString(arena.bluecp + 1) + "  "));
+			ascore.get(arena.getInternalName()).resetScores(Bukkit.getOfflinePlayer(Integer.toString(arena.redcp - 1) + " "));
+			ascore.get(arena.getInternalName()).resetScores(Bukkit.getOfflinePlayer(Integer.toString(arena.redcp + 1) + " "));
+			ascore.get(arena.getInternalName()).resetScores(Bukkit.getOfflinePlayer(Integer.toString(arena.bluecp - 1) + "  "));
+			ascore.get(arena.getInternalName()).resetScores(Bukkit.getOfflinePlayer(Integer.toString(arena.bluecp + 1) + "  "));
 
-			aobjective.get(arena.getName()).getScore(Bukkit.getOfflinePlayer(ChatColor.AQUA + "CHECKPOINTS:")).setScore(5);
-			aobjective.get(arena.getName()).getScore(Bukkit.getOfflinePlayer(ChatColor.RED + "RED:")).setScore(4);
-			aobjective.get(arena.getName()).getScore(Bukkit.getOfflinePlayer(Integer.toString(arena.redcp) + " ")).setScore(3);
-			aobjective.get(arena.getName()).getScore(Bukkit.getOfflinePlayer(ChatColor.BLUE + "BLUE:")).setScore(2);
-			aobjective.get(arena.getName()).getScore(Bukkit.getOfflinePlayer(Integer.toString(arena.bluecp) + "  ")).setScore(1);
-			p.setScoreboard(ascore.get(arena.getName()));
+			aobjective.get(arena.getInternalName()).getScore(Bukkit.getOfflinePlayer(ChatColor.AQUA + "CHECKPOINTS:")).setScore(5);
+			aobjective.get(arena.getInternalName()).getScore(Bukkit.getOfflinePlayer(ChatColor.RED + "RED:")).setScore(4);
+			aobjective.get(arena.getInternalName()).getScore(Bukkit.getOfflinePlayer(Integer.toString(arena.redcp) + " ")).setScore(3);
+			aobjective.get(arena.getInternalName()).getScore(Bukkit.getOfflinePlayer(ChatColor.BLUE + "BLUE:")).setScore(2);
+			aobjective.get(arena.getInternalName()).getScore(Bukkit.getOfflinePlayer(Integer.toString(arena.bluecp) + "  ")).setScore(1);
+			p.setScoreboard(ascore.get(arena.getInternalName()));
 		}
 	}
 
 	@Override
 	public void updateScoreboard(JavaPlugin plugin, final Arena arena) {
-		IArena a = (IArena) MinigamesAPI.getAPI().pinstances.get(plugin).getArenaByName(arena.getName());
+		IArena a = (IArena) MinigamesAPI.getAPI().pinstances.get(plugin).getArenaByName(arena.getInternalName());
 		this.updateScoreboard(a);
 	}
 
